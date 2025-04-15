de9aa3c2ae147836bd94d6d4dc0e170ea043658a
Grace Jeong <dbwjd1742@gmail.com>
2025-04-14 17:34:07 -0700

    Completed Analysis

--- Kaggle/titanic/utils.py
+++ Kaggle/titanic/utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import joblib
 
 import matplotlib.pyplot as plt
 import seaborn as sns
@@ -17,27 +18,36 @@ from sklearn.model_selection import cross_val_score, GridSearchCV, RandomizedSea
 from imblearn.over_sampling import SMOTE
 from statistics import mean
 
-def preprocess_data(data):
+def preprocess_data(data, is_train=True):
+    
+    data = data.copy()
+    
+    passenger_ids = data['PassengerId'] if not is_train else None
+    
+    data['TicketCode'] = data['Ticket'].str.extract(r'(\d+)', expand=False)
+    data['TicketCode'] = data['TicketCode'].fillna('Unknown')
+
     # Fill missing 'Age' values based on the median of each 'Pclass'
     data['Age'] = data.groupby('Pclass')['Age'].transform(lambda x: x.fillna(x.median()))
 
     # Fill missing 'Fare' values with the median
     data['Fare'] = data['Fare'].fillna(data['Fare'].median())
-
-    # Drop columns that won't be useful for the analysis
-    data.drop(columns=['Cabin', 'Name', 'Ticket'], inplace=True)
-
+  
     # Fill missing 'Embarked' values with the mode (most frequent value)
     data['Embarked'] = data['Embarked'].fillna(data['Embarked'].mode()[0])
 
     # Create a 'FamilySize' column by adding 'SibSp' (siblings/spouses aboard) and 'Parch' (parents/children aboard) + 1 for the individual
     data['FamilySize'] = data['SibSp'] + data['Parch'] + 1  # +1 for self
-    data.drop(columns=['SibSp', 'Parch'], inplace=True)
-    
     # Create an 'IsAlone' column where 1 means the person is alone (FamilySize == 1), otherwise 0
     data['IsAlone'] = (data['FamilySize'] == 1).astype(int)
-   
-    return data
+    
+    drop_cols = ['Cabin', 'Name', 'Ticket', 'SibSp', 'Parch', 'PassengerId']
+    data = data.drop(columns=[col for col in drop_cols if col in data.columns])
+    
+    if not is_train:
+        return data, passenger_ids
+    else:
+        return data
 
 def encode_feature(data):
  
@@ -45,8 +55,9 @@ def encode_feature(data):
     data['Sex'] = data['Sex'].map({'male': 0, 'female': 1}).astype(int)
 
     # One-hot encoding for 'Embarked'
-    data = pd.get_dummies(data, columns=['Embarked']).astype(int)
-  
+    data = pd.get_dummies(data, columns=['Embarked'], drop_first=True)
+    data[['Embarked_Q', 'Embarked_S']] = data[['Embarked_Q', 'Embarked_S']].astype(int)
+
     return data
 
 def detect_outliers_iqr(data, column):
@@ -56,6 +67,28 @@ def detect_outliers_iqr(data, column):
     lower_bound = Q1 - 1.5 * IQR
     upper_bound = Q3 + 1.5 * IQR
     return data, data[(data[column] < lower_bound) | (data[column] > upper_bound)]
+
+def splitting_data(data, return_all=True):
+
+    X = data.drop('Survived', axis=1)
+    Y = data['Survived']
+
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=42)
+    print(f"Size of training data: {X_train.shape[0]}")
+    print(f"Size of test data: {X_test.shape[0]}")
+
+    X_train_scaled = X_train.copy()
+    X_test_scaled = X_test.copy()
+
+    cols_to_scale = ['Age', 'Fare', 'FamilySize']
+    rs = RobustScaler()
+
+    X_train_scaled[cols_to_scale] = rs.fit_transform(X_train[cols_to_scale])
+    X_test_scaled[cols_to_scale] = rs.transform(X_test[cols_to_scale])
+    
+    joblib.dump(rs, 'scaler.pkl')
+    
+    return X_train_scaled, X_test_scaled, Y_train, Y_test  
 
 def evaluate_model(model, X_train_scaled, X_test_scaled, Y_train, Y_test):
 
